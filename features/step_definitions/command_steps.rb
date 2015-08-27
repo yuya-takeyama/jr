2bc95a6ff410086c029fda154d8a9a9920b451e0
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2015-08-28 04:23:59 +0900

    Adopt for aruba 0.9.0

--- features/step_definitions/command_steps.rb
+++ features/step_definitions/command_steps.rb
@@ -1,4 +1,4 @@
 When(/^I run `([^`]*)` in bash$/)do |cmd|
-  cmd = "bash -c '%s'" % Aruba::Platform.unescape(cmd, aruba.config.keep_ansi)
+  cmd = "bash -c '%s'" % unescape_text(unescape_text(cmd))
   run_simple(cmd, false)
 end
