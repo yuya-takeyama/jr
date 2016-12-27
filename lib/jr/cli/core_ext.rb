3d15314c76adce2d7848bb44d3c4e5795f9b22b8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-12-28 02:03:19 +0900

    Remove Kernel#itself
    Now it's implemented in Ruby 2.2 and later versions

--- lib/jr/cli/core_ext.rb
+++ lib/jr/cli/core_ext.rb
@@ -1,4 +1,3 @@
-require 'jr/cli/core_ext/kernel'
 require 'jr/cli/core_ext/hash'
 require 'jr/cli/core_ext/enumerable'
 require 'jr/cli/core_ext/enumerator'
--- lib/jr/cli/core_ext/kernel.rb
+++
@@ -1,7 +0,0 @@
-module Kernel
-  unless Kernel.respond_to? :itself
-    def itself
-      self
-    end
-  end
-end
